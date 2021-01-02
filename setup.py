7338229655fe0e4964999044939cb8d173b1f22f
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-02 13:15:08 +0300

    Updated e-mail and url.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='NlpToolkit-ParseTree',
     version='1.0.5',
     packages=['ParseTree', 'ParseTree.NodeCondition'],
-    url='https://github.com/olcaytaner/ParseTree-Py',
+    url='https://github.com/StarlangSoftware/ParseTree-Py',
     license='',
     author='olcaytaner',
-    author_email='olcaytaner@isikun.edu.tr',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Constituency Parse Tree Library',
     install_requires = ['NlpToolkit-Dictionary']
 )
