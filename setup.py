c10ff77fd198a0b4224027d7168de76ad0119935
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 20:05:03 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-ParseTree',
-    version='1.0.7',
+    version='1.0.8',
     packages=['ParseTree', 'ParseTree.NodeCondition'],
     url='https://github.com/StarlangSoftware/ParseTree-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Constituency Parse Tree Library',
-    install_requires = ['NlpToolkit-Dictionary']
+    install_requires = ['NlpToolkit-Dictionary'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
